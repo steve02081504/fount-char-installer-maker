09769b27c6c6dfca30a01e4b96e939ca6e6d2897
steve02081504 <steve02081504@EDEN.fukwold>
2025-11-09 22:27:19 +0800

    fixes

--- char_installer_maker.ps1
+++ char_installer_maker.ps1
@@ -3,7 +3,7 @@
 $char_url = Read-Host -Prompt "输入你的角色地址（传统酒馆角色的png的下载地址或risu的charx角色下载地址或fount角色的安装包下载地址或fount角色的git地址）"
 $fount_url = "fount://run/shells/install/install;$([uri]::EscapeDataString($char_url))"
 @"
-if (!(get-command "fount" -ErrorAction SilentlyContinue)) {
+if (!(Get-Command "fount.bat" -ErrorAction SilentlyContinue)) {
 	# prompt for choice
 	`$result = `$Host.UI.PromptForChoice(
 		"未检测到fount",
@@ -12,13 +12,15 @@ if (!(get-command "fount" -ErrorAction SilentlyContinue)) {
 		0
 	)
 	if (`$result -eq 0) {
-		`$scriptContent = Invoke-RestMethod https://raw.githubusercontent.com/steve02081504/fount/refs/heads/master/src/runner/main.ps1
-		Invoke-Expression "function fountInstaller { `$scriptContent }"
-		fountInstaller open protocolhandle "$fount_url"
+		powershell.exe -noprofile -executionpolicy bypass -command @"
+```$scriptContent = Invoke-RestMethod https://raw.githubusercontent.com/steve02081504/fount/refs/heads/master/src/runner/main.ps1
+Invoke-Expression "function fountInstaller { ```$scriptContent }"
+fountInstaller open protocolhandle "$fount_url"
+`"@
 	}
 }
 else {
-	fount open protocolhandle "$fount_url"
+	Start-Process fount.bat @("open", "protocolhandle", "$fount_url")
 }
-"@ | ps12exe *>&1 | Out-Null
+"@ | ps12exe -noConsole *>&1 | Out-Null
 "编译成功！"
